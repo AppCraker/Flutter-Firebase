3bd05da39b911456a5a868cd9581aa40b319312f
AppCraker <www.krishnachavda9@gmail.com>
2024-10-22 00:08:43 +0530

    keep me login completed

--- lib/screens/posts/post_screen.dart
+++ lib/screens/posts/post_screen.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:testingapp/screens/login/loginpage.dart';
 
 class PostScreen extends StatefulWidget {
   const PostScreen({super.key});
@@ -8,11 +10,29 @@ class PostScreen extends StatefulWidget {
 }
 
 class _PostScreenState extends State<PostScreen> {
+  final auth = FirebaseAuth.instance;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Post"),
+        title: Text("Post",textAlign: TextAlign.center,),
+        actions: [
+          IconButton(
+            onPressed: (){
+              auth.signOut().then((value){
+                Navigator.push(context, MaterialPageRoute(builder: (context)=> Loginpage()));
+              }).onError((error,stackTrace){
+                ScaffoldMessenger.of(context).showSnackBar(
+                                   SnackBar(
+                                         content: Text(error.toString()),
+                                          backgroundColor: Colors.red,
+                                          duration: Duration(seconds: 3),
+                                   ),
+                                 );
+              });
+            }, 
+            icon: Icon(Icons.logout_outlined))
+        ],
       ),
     );
   }
--- lib/screens/splash/splashScreen.dart
+++ lib/screens/splash/splashScreen.dart
@@ -1,19 +1,42 @@
 import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
 import 'package:testingapp/screens/login/loginpage.dart';
+import 'package:testingapp/screens/posts/post_screen.dart';
 
-class animateSplash extends StatelessWidget {
-  const animateSplash({super.key});
+class animateSplash extends StatefulWidget {
+  animateSplash({super.key});
+
+  @override
+  State<animateSplash> createState() => _animateSplashState();
+}
+
+class _animateSplashState extends State<animateSplash> {
+  final auth = FirebaseAuth.instance;
+
+  
 
   @override
   Widget build(BuildContext context) {
-    return AnimatedSplashScreen(
+    final user = auth.currentUser;
+    if (user!= null) {
+       return AnimatedSplashScreen(
+      splash: LottieBuilder.asset("assets/lottie/Animation - 1729352780895.json"),
+      nextScreen: const PostScreen(),
+      splashIconSize: 300,
+      duration: 3000,  // Optional: Set a duration for the splash
+      backgroundColor: Colors.white,  // Add background color to avoid a white screen
+    );
+    }else{
+       return AnimatedSplashScreen(
       splash: LottieBuilder.asset("assets/lottie/Animation - 1729352780895.json"),
       nextScreen: const Loginpage(),
       splashIconSize: 300,
       duration: 3000,  // Optional: Set a duration for the splash
       backgroundColor: Colors.white,  // Add background color to avoid a white screen
     );
+    }
+   
 }
 }
